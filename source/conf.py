9ee8748360f06331705d4163b433c73b24df9d92
Jakub Steiner <jimmac@gmail.com>
2021-03-30 15:10:53 +0200

    logo

--- source/conf.py
+++ source/conf.py
@@ -55,7 +55,7 @@ html_theme_options = {
         "color-brand-content": "#4a86cf",
     }
 }
-html_logo = "logo.png"
+html_logo = "img/logo.svg"
 # add custom files that are stored in _static
 html_css_files = ['gnome.css']
 
